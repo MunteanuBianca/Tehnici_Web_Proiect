22e0bb2d39e065f3f6c3551c718a2d9a40f566d1
MunteanuBianca <ioana.2013.bm@gmail.com>
2022-05-12 17:22:21 +0300

    added time spent on page

--- Hotel_pagina_principala.js
+++ Hotel_pagina_principala.js
@@ -1,17 +1,32 @@
 function reveal() {
-    var reveals = document.getElementsByClassName("reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.getElementsByClassName("reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
+
+let seconds = 0;
+let intervalId = null;
+
+function toggleInterval() {
+  const stop = intervalId != null;
+    intervalId = setInterval(() => {
+      seconds++;
+      document.querySelector('p').textContent = `Stai pe această pagină de ${seconds} secunde.`;
+    }, 1000);
+}
+
+window.onload = () => {
+  toggleInterval();
+}
+
+window.addEventListener("scroll", reveal);
